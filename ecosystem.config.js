93b02a5840746f2e1f4a998f9b11960dabf1c789
fpigeonjr <frank.pigeonjr@gmail.com>
2023-07-09 15:13:37 -0500

    update to add folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,20 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "strapi",
+//       cwd: "/srv/strapi/",
+//       script: "npm",
+//       args: "start",
+//       autorestart: true,
+//       env: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//   ],
+// };
+
+const fs = require("fs");
+
 module.exports = {
   apps: [
     {
@@ -9,6 +26,10 @@ module.exports = {
       env: {
         NODE_ENV: "production",
       },
+      post_install:
+        "mkdir -p /app/public/uploads && chmod -R 777 /app/public/uploads",
+      post_update:
+        "mkdir -p /app/public/uploads && chmod -R 777 /app/public/uploads",
     },
   ],
 };
